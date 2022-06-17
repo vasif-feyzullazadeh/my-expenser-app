58febaa0ae23ca15642984ec9952bea44363762c
vasif-feyzullazadeh <vasif.feyzullazade@gmail.com>
2022-06-17 14:28:31 +0400

    app uploaded

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,9 @@
+// reducer: bir funksiyadir State Action ve Parametri alir ve axirda state qaytarir 
+
+
+
+const userReducer = (state, action) => {
+    return state;
+}
+
+export default userReducer;
