f6aebc1faf9929beea9793ab7f56fb1516f85996
vasif-feyzullazadeh <vasif.feyzullazade@gmail.com>
2022-06-17 15:00:20 +0400

    continue

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,9 +1,18 @@
-// reducer: bir funksiyadir State Action ve Parametri alir ve axirda state qaytarir 
+import { User, UserAction, UserState } from './../../types/user';
 
+const defaultState: UserState = {
+    data: {} as User,
+    loading: false,
+    error: ""
+}
 
-
-const userReducer = (state, action) => {
+const userReducer = (state: UserState, action: UserAction) => {
     return state;
 }
 
 export default userReducer;
+
+
+
+
+// reducer: bir funksiyadir State Action ve Parametri alir ve axirda state qaytarir 
